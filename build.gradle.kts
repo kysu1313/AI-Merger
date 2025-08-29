e649ed190f676d483ce739fb806251f8b5eb4b49
Kyle Supple <38538913+kysu1313@users.noreply.github.com>
2025-08-28 23:26:02 -0400

    add support for 251.* Rider IDE versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.2.10"
+    kotlin("jvm") version "2.1.10"
     id("org.jetbrains.intellij.platform") version "2.7.2"
 }
 
@@ -10,23 +10,40 @@ repositories {
 
 dependencies {
     intellijPlatform {
-        rider("2025.2") {
+        rider("2025.1") {
             useInstaller.set(false)
         }
         bundledPlugin("Git4Idea")
     }
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+intellijPlatform {
+    pluginConfiguration {
+        name = "Multi-Repo Merger"
+        version = "0.1.0"
+        ideaVersion {
+            sinceBuild.set("251.0")
+            untilBuild.set("251.*")
+        }
+        vendor {
+            name = "Kyle Supple"
+            email = "kylems3376@gmail.com"
+            url = "https://code-nerd.com"
+        }
     }
 }
 
+java {
+    toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
+}
 kotlin {
     jvmToolchain(17)
 }
 
 tasks.runIde {
-    environment("OPENAI_API_KEY", System.getenv("OPENAI_API_KEY"))
+    val k = System.getenv("OPENAI_API_KEY")
+    if (k != null) {
+        environment("OPENAI_API_KEY", k)
+        jvmArgs("-DOPENAI_API_KEY=$k")
+    }
 }
